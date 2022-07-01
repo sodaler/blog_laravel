4d5730f9b1f2055e6f9c01f83b64ca0ef96f677e
sodaler <misha_lector87@mail.ru>
2022-07-01 15:39:30 +0300

    add Main namespace

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', 'IndexController');
 });
 
 Auth::routes();
