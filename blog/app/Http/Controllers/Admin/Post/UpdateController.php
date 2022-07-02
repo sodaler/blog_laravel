c4a04b2798a3d1e4654f22256dce02f98931d3de
sodaler <misha_lector87@mail.ru>
2022-07-03 01:56:38 +0400

    temporary changes for posts update

--- blog/app/Http/Controllers/Admin/Post/UpdateController.php
+++ blog/app/Http/Controllers/Admin/Post/UpdateController.php
@@ -12,14 +12,21 @@ class UpdateController extends Controller
     public function __invoke(UpdateRequest $request, Post $post)
     {
         $data = $request->validated();
-        $tagIds = $data['tag_ids'];
-        unset($data['tag_ids']);
+        if (isset($data['tag_ids'])) {
+            $tagIds = $data['tag_ids'];
+            unset($data['tag_ids']);
+        }
 
-        $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
-        $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
-        $post = Post::firstOrCreate($data);
+        if (isset($data['preview_image'])) {
+            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+        }
+        if (isset($data['main_image'])) {
+            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
+        }
         $post->update($data);
-        $post->tags()->sync($tagIds);
+        if (isset($tagIds)) {
+            $post->tags()->sync($tagIds);
+        }
         return view('admin.post.show', compact('post'));
     }
 }
