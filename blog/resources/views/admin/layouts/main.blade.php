8e0ea8bfe5bdc000c6534376315ce3afe91ed240
sodaler <misha_lector87@mail.ru>
2022-07-02 19:22:42 +0300

    add attributes for images upload

--- blog/resources/views/admin/layouts/main.blade.php
+++ blog/resources/views/admin/layouts/main.blade.php
@@ -70,6 +70,7 @@
 <!-- daterangepicker -->
 <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
 <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
+<script src="{{ asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
 <!-- overlayScrollbars -->
 <script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
@@ -87,6 +88,10 @@
                 ['height', ['height']]
             ]
         });
+    });
+
+    $(function () {
+        bsCustomFileInput.init();
     });
 </script>
 </body>
