5174f3a5b3165d41936121970b5d0ec7b45b74cc
sodaler <misha_lector87@mail.ru>
2022-07-02 19:51:09 +0300

    bind categories to post

--- blog/resources/views/admin/layouts/main.blade.php
+++ blog/resources/views/admin/layouts/main.blade.php
@@ -26,7 +26,8 @@
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
+        <img class="animation__shake" src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60"
+             width="60">
     </div>
 
     <!-- Navbar -->
@@ -76,7 +77,7 @@
 <!-- AdminLTE App -->
 <script src="{{ asset('dist/js/adminlte.js') }}"></script>
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('#summernote').summernote({
             toolbar: [
                 // [groupName, [list of button]]
@@ -94,5 +95,10 @@
         bsCustomFileInput.init();
     });
 </script>
+<style>
+    .custom-file-input:lang(en) ~ .custom-file-label::after {
+        content: "...";
+    }
+</style>
 </body>
 </html>
--- blog/resources/views/admin/post/create.blade.php
+++ blog/resources/views/admin/post/create.blade.php
@@ -46,6 +46,9 @@
                                         <span class="input-group-text">Загрузить</span>
                                     </div>
                                 </div>
+                                @error('preview_image')
+                                <div class="text-danger">Это поле необходимо заполнить</div>
+                                @enderror
                             </div>
                             <div class="form-group w-50">
                                 <label for="exampleInputFile">Добавить главное изображение</label>
@@ -58,6 +61,19 @@
                                         <span class="input-group-text">Загрузить</span>
                                     </div>
                                 </div>
+                                @error('main_image')
+                                <div class="text-danger">Это поле необходимо заполнить</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Выберите категорию</label>
+                                <select name="category_id" class="form-control">
+                                    @foreach($categories as $category)
+                                    <option value="{{ $category->id }}"
+                                    {{ $category->id == old('category_id') ? ' selected' : ''}}
+                                    >{{ $category->title }}</option>
+                                    @endforeach
+                                </select>
                             </div>
                             <div class="form-group">
                                 <input type="submit" class="btn btn-primary" value="Добавить">
