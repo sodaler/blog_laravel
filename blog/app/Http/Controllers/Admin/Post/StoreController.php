a5acf8e46654fffb152a5b1b23a4958c0711d239
sodaler <misha_lector87@mail.ru>
2022-07-03 02:20:33 +0400

    add service and base controller

--- blog/app/Http/Controllers/Admin/Post/StoreController.php
+++ blog/app/Http/Controllers/Admin/Post/StoreController.php
@@ -7,22 +7,13 @@ use App\Http\Requests\Admin\Post\StoreRequest;
 use App\Models\Post;
 use Illuminate\Support\Facades\Storage;
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
     public function __invoke(StoreRequest $request)
     {
-        try {
-            $data = $request->validated();
-            $tagIds = $data['tag_ids'];
-            unset($data['tag_ids']);
+        $data = $request->validated();
+        $this->service->store($data);
 
-            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
-            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
-            $post = Post::firstOrCreate($data);
-            $post->tags()->attach($tagIds);
-        } catch(\Exception $exception) {
-            abort(404);
-        }
         return redirect()->route('admin.post.index');
     }
 }
