a0a5f8856c94b5c09284b219bb548f6b471558de
sodaler <misha_lector87@mail.ru>
2022-07-03 00:51:24 +0400

    bind tags to post

--- blog/resources/views/admin/post/create.blade.php
+++ blog/resources/views/admin/post/create.blade.php
@@ -75,6 +75,14 @@
                                     @endforeach
                                 </select>
                             </div>
+                            <div class="form-group w-50">
+                                <label>Теги</label>
+                                <select class="select2" multiple="multiple" name="tag_ids[]" data-placeholder="Выберите теги" style="width: 100%;">
+                                    @foreach($tags as $tag)
+                                    <option {{ is_array(old('tag_ids')) && in_array($tag->id, old('tag_ids')) ? ' selected' : '' }} value="{{ $tag->id }}">{{ $tag->title }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                             <div class="form-group">
                                 <input type="submit" class="btn btn-primary" value="Добавить">
                             </div>
