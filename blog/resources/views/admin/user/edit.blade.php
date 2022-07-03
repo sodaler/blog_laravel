6019e4ab0acb8ac95fb7aa966632a0bd75724254
sodaler <misha_lector87@mail.ru>
2022-07-03 20:30:09 +0400

    roles for users

--- blog/resources/views/admin/user/edit.blade.php
+++ blog/resources/views/admin/user/edit.blade.php
@@ -37,6 +37,22 @@
                                 <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
+                            <div class="form-group w-50">
+                                <label>Выберите роль</label>
+                                <select name="role" class="form-control">
+                                    @foreach($roles as $id => $role)
+                                        <option value="{{ $id }}"
+                                            {{ $id == $user->role ? ' selected' : ''}}
+                                        >{{ $role }}</option>
+                                    @endforeach
+                                </select>
+                                @error('role')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <input type="hidden" name="user_id" value="{{ $user->id }}">
+                            </div>
                             <input type="submit" class="btn btn-primary" value="Обновить">
                         </form>
                     </div>
