1ab0a5ccabedbff4e748b8a77a7459ba084cd036
sodaler <misha_lector87@mail.ru>
2022-07-05 19:17:23 +0400

    add dates and relatedPosts

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -26,4 +26,7 @@ class Post extends Model
         return $this->belongsToMany(User::class, 'post_user_likes', 'post_id', 'user_id');
     }
 
+    public function comments() {
+        return $this->hasMany(Comment::class, 'post_id', 'id');
+    }
 }
